59fefdf29c7074bfff36065396b47839a67ece09
Alex Vergara <68871459+Pyzyryab@users.noreply.github.com>
2022-10-19 17:47:10 +0200

    closes #12 Gh 12 generate the first action of the project to setup a GitHub folder (#16)
    * Create pylint.yml
    
    * Update pylint.yml
    
    Only using v3.10 of Python for the analisys
    
    * Solving Pyling warnings (#15)
    
    * Solving Pyling warnings on zork++ module
    
    * Fixed linter warnings on main file. Disabled C0103 and W0511
    
    * Warnings on setup.py. Filled extra data and install requires clause
    
    * Added .pylintrc file to configure plugin on action and on local instances of code editors
    
    * New bulk of Pylint warnings corrected
    
    * Upgraded the .pylintrc file
    
    * Pyling warnings fixes

--- zork/data/attributes.py
+++ zork/data/attributes.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass
-
 """[summary]
     Classes for store constant data about the internal configuration
     (elected by design) of the program attributes and properties
 """
+
+from dataclasses import dataclass
 
 
 @dataclass
