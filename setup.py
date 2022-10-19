7e5d98df36c2d5a7b7cbe42b24aeb3c586780de4
Alex Vergara <pyzyryab@tutanota.com>
2022-10-19 10:23:32 +0200

    [Documentation] - Minimal update on the setup.py file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zork-manager',
-    version='0.3.0',
+    name='zork++',
+    version='1.0.0',
     license='MIT',
     author="Alex Vergara",
     author_email='pyzyryab@tutanota.com',
     packages=find_packages('zork'),
     package_dir={'': 'zork'},
-    url='https://github.com/Pyzyryab/Zork',
-    keywords='C++ project manager',
+    url='https://github.com/zerodaycode/Zork',
+    keywords='C++ project manager build system',
     install_requires=[
-        #   '',
-      ],
+      'pyinstaller'
+    ],
 )
