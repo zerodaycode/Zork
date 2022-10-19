d7fb1ba75c3839a579b005d6f81d40dc64a57fbd
Alex Vergara <pyzyryab@tutanota.com>
2022-10-19 13:34:10 +0200

    Warnings on setup.py. Filled extra data and install requires clause

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+""" _summary_
+
+    Provides the configuration for build the project with `Pyinstaller` as an executable
+"""
+
 from setuptools import setup, find_packages
 
 setup(
@@ -8,9 +13,9 @@ setup(
     author_email='pyzyryab@tutanota.com',
     packages=find_packages('zork'),
     package_dir={'': 'zork'},
-    url='https://github.com/Pyzyryab/Zork',
-    keywords='C++ project manager',
+    url='https://github.com/zerodaycode/Zork',
+    keywords='C++ project manager and build system',
     install_requires=[
-        #   '',
-      ],
+		'pyinstaller'
+	],
 )
