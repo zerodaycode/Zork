be1575560e742ccb3c04b305d66c47cce54eed65
OnSystem <48860619+TheHiddenOnSystem@users.noreply.github.com>
2022-12-30 10:32:44 +0100

    Added config logger (#3)
     * Implementation of the logger and the CLI parser

--- zork-rust/src/bin/main.rs
+++ zork-rust/src/bin/main.rs
@@ -1,4 +1,22 @@
+use env_logger::Target;
+use zork::{config_cli::CliArgs, utils::logger::config_logger};
+use clap::Parser;
+
 
 fn main() {
-    println!("Hello, world!");
+    let parser_cli = CliArgs::parse_from([""]);
+
+    config_logger(parser_cli.verbose, Target::Stdout)
+        .expect("Error configure logger");
+
+
+    
+    log::warn!("warn");
+    log::info!("info");
+    log::error!("error");
+
 }
+
+
+
+
