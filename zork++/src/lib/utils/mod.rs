bad39245c60b54ff0ad6dce3a9f5d7cea2f6d040
Marius Hirt <marius-hirt@web.de>
2023-01-08 18:36:42 +0000

    Extract template project into own resources module Keeping the templates in their own files makes them more maintainable.

--- zork++/src/lib/utils/mod.rs
+++ zork++/src/lib/utils/mod.rs
@@ -1,4 +1,3 @@
-pub mod constants;
 pub mod logger;
 pub mod reader;
 pub mod template;
---
+++ zork++/src/lib/utils/template/resources/mod.rs
@@ -0,0 +1,9 @@
+pub const ROOT_PATH_NAME: &str = "example";
+pub const PROJECT_NAME: &str = "calculator";
+pub const CONFIG_FILE_NAME: &str = "zork.toml"; // zork.toml
+
+pub const IFC_MOD_FILE: &str = include_str!("math.cppm");
+pub const SRC_MOD_FILE: &str = include_str!("math.cpp");
+pub const SRC_MOD_FILE_2: &str = include_str!("math2.cpp");
+pub const MAIN: &str = include_str!("main.cpp");
+pub const CONFIG_FILE: &str = include_str!("zork.toml");
