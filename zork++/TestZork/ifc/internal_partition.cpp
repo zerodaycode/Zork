a69b12ed6928fb6450be661b4778d954f5963826
Alex Vergara <pyzyryab@tutanota.com>
2023-02-26 10:34:19 +0100

    Cleaning examples

--- zork++/TestZork/ifc/internal_partition.cpp
+++
@@ -1,7 +0,0 @@
-module partitions:internal_partition;
-
-// A module implementation may implement a declaration imported from
-// it's relative module interface, but shall not export anything
-int just_a_42() {
-    return 42;
-}
