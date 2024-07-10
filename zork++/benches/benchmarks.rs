683444d244adeb32b133c2c7de5bef8185e96e15
Alex Vergara <alex.vergara.dev@gmail.com>
2024-07-10 21:22:57 +0200

    fix: changed the arguments passed to the bencher function on the measures of the 'Generate Commands'

--- zork++/benches/benchmarks.rs
+++ zork++/benches/benchmarks.rs
@@ -7,7 +7,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use zork::{
     cache::{self, ZorkCache},
     cli::input::CliArgs,
-    compiler::build_project,
     config_file::{self, ZorkConfigFile},
     utils::{self, reader::build_model},
 };
@@ -18,10 +17,9 @@ pub fn build_project_benchmark(c: &mut Criterion) {
         config_file::zork_cfg_from_file(utils::constants::CONFIG_FILE_MOCK).unwrap();
     let cli_args = CliArgs::parse();
     let program_data = build_model(config, &cli_args, Path::new(".")).unwrap();
-    let cache = ZorkCache::default();
 
-    c.bench_function("Build project", |b| {
-        b.iter(|| generate_commands(black_box(&program_data), black_box(cache), &cli_args))
+    c.bench_function("Generate commands", |b| {
+        b.iter(|| generate_commands(black_box(&program_data), black_box(ZorkCache::default()), &cli_args))
     });
 
     c.bench_function("Cache loading time", |b| {
